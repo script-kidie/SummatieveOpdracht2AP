3bd3fb17539a5ecad6f7a06dc4f5a54e0c82a88d
script-kidie <74268141+script-kidie@users.noreply.github.com>
2021-05-17 15:07:09 +0200

    Added documentation in FSM class

--- src/FiniteStateMachine/FSM.java
+++ src/FiniteStateMachine/FSM.java
@@ -27,18 +27,26 @@ public class FSM {
 
 
     public int runText(){
+        /**
+         * Jumps between nodes based if a text input
+         * @return int
+         */
 
-        this.currentNode = nodeArray.get(0);
-        List<String> history = new ArrayList<>();
-        history.add(currentNode.getName());
 
+        this.currentNode = nodeArray.get(0); //start at the first node
+
+        List<String> history = new ArrayList<>(); //make an empty list to track nodes that the code has jumped to
+
+        history.add(currentNode.getName()); // add the current nodes name to the history list
+
+        // loop true all the characters in the given "jumps" string
         for(char ch: jumps.toCharArray()){
 
-            String c = String.valueOf(ch);
+            String c = String.valueOf(ch);  //make "ch" a string for easier use
 
-            if (currentNode.getJumps().containsKey(c)) {
-                currentNode = currentNode.getJumps().get(c);
-                history.add(currentNode.getName());
+            if (currentNode.getJumps().containsKey(c)) {    // check if the current node contains the given key
+                currentNode = currentNode.getJumps().get(c); // get the next node
+                history.add(currentNode.getName()); // add the current nodes name to the history list
 
             } else {
                 System.out.println("//-- Error melding --//");
@@ -53,26 +61,31 @@ public class FSM {
     }
 
     public int runChance(){
+        /**
+         * Jumps between nodes at random
+         * @return int
+         */
 
 
-        Random randInt = new Random();
+        Random randInt = new Random(); // make a random int
+
+        this.currentNode = nodeArray.get(randInt.nextInt(nodeArray.size())); // get a random node from the ones available
+
+        List<String> history = new ArrayList<>(); //make an empty list to track nodes that the code has jumped to
+
+        history.add(currentNode.getName()); // add the current nodes name to the history list
+
+        Object[] keys = currentNode.getJumps().keySet().toArray(); // get all keys from the first random selcted node
 
 
-        this.currentNode = nodeArray.get(randInt.nextInt(nodeArray.size()));
-
-        List<String> history = new ArrayList<>();
-
-        history.add(currentNode.getName());
-
-        Object[] keys = currentNode.getJumps().keySet().toArray();
-
+        // loops for the amount of keys in the current node
         for (Object val: keys){
-            Object randomkey = keys[randInt.nextInt(keys.length)];
+            Object randomkey = keys[randInt.nextInt(keys.length)]; // get a random key from the ones available
             System.out.println(randomkey);
 
-            if (currentNode.getJumps().containsKey(randomkey)) {
-                currentNode = currentNode.getJumps().get(randomkey);
-                history.add(currentNode.getName());
+            if (currentNode.getJumps().containsKey(randomkey)) {  // check if the current node contains the given key
+                currentNode = currentNode.getJumps().get(randomkey); // get the next node
+                history.add(currentNode.getName()); // add the current nodes name to the history list
 
             } else {
                 System.out.println("//-- Error melding --//");
